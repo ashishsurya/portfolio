5109f590187388a0d8c6ae91c22e6ea3aa099511
Surya Ashish <ashishsurya126@gmail.com>
2023-08-27 23:41:00 +0530

    portfolio started

---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <header className='flex justify-center py-3 md:py-5 border-b w-full '>
+      <h3 className='   text-xl md:text-4xl  w-fit font-semibold'>
+        Surya Ashish
+      </h3>
+    </header>
+  );
+};
---
+++ src/components/tagline.tsx
@@ -0,0 +1,15 @@
+export const Tagline = () => {
+  return (
+    <div className='flex text-sm md:text-base space-x-2 text-slate-600'>
+      <p>React</p>
+      <p>&middot;</p>
+      <p>MERN</p>
+      <p>&middot;</p>
+      <p>NodeJS</p>
+      <p>&middot;</p>
+      <p>TailwindCSS</p>
+      <p>&middot;</p>
+      <p>C++</p>
+    </div>
+  );
+};
